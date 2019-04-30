dbd65453640f683d1c8f75da0712749cfdb7c539
Jeff Butler <jeffgbutler@gmail.com>
2019-04-30 17:32:53 -0400

    Better Java instructions and examples
    No need for manually configuring Spring Cloud Connectors. On PCF, the
    Spring Auto Reconfiguration will do it all

--- loan-calculator-redis/src/main/java/com/jgb/loancalculator/CloudConfig.java
+++ loan-calculator-redis/src/main/java/com/jgb/loancalculator/CloudConfig.java
@@ -1,6 +1,8 @@
 package com.jgb.loancalculator;
 
-import org.springframework.cloud.config.java.AbstractCloudConfig;
+import com.jgb.loancalculator.service.CounterService;
+import com.jgb.loancalculator.service.CounterServiceRedis;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,22 +10,14 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
-import com.jgb.loancalculator.service.CounterService;
-import com.jgb.loancalculator.service.CounterServiceRedis;
-
 @Configuration
 @Profile("cloud")
-public class CloudConfig extends AbstractCloudConfig {
+public class CloudConfig {
 
     @Bean
-    public RedisConnectionFactory redisFactory() {
-        return connectionFactory().redisConnectionFactory();
-    }
-    
-    @Bean
-    public RedisTemplate<String, Integer> redisTemplate() {
+    public RedisTemplate<String, Integer> redisTemplate(RedisConnectionFactory redisFactory) {
         RedisTemplate<String, Integer> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisFactory());
+        template.setConnectionFactory(redisFactory);
         template.setValueSerializer(new GenericToStringSerializer<>(Integer.class));
         return template;
     }
